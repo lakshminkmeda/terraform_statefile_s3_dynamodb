3e11d295250c5aa66b96aa7f9e5bbae248f8998f
lakshminkmeda <lakshmi.nareen@gmail.com>
2024-10-27 17:31:55 +0000

    First Terraform Code

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.73.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tf-backend-s3-bucket"
+    key = "terraform/terraform.tfstate"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "oncloud9-bucket"
+}
